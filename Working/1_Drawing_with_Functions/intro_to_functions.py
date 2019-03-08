4199e126aec5eec3065920c10a192421b2372bc9
robuntu <robuntu@stro.ycdsb.ca>
2019-03-08 11:05:28 -0500

    drawing fucntions

--- Working/1_Drawing_with_Functions/intro_to_functions.py
+++ Working/1_Drawing_with_Functions/intro_to_functions.py
@@ -5,9 +5,9 @@ SCREEN_HEIGHT = 600
 
 # define your draw functions
 def draw_cloud():
-    arcade.draw_circle_filled(position_x - 65, 500, 50, arcade.color.WHITE)
-    arcade.draw_circle_filled(position_x, 500, 50, arcade.color.WHITE)
-    arcade.draw_circle_filled(position_x + 65, 500, 50, arcade.color.WHITE)
+    arcade.draw_circle_filled(position_cloud - 65, 500, 50, arcade.color.WHITE)
+    arcade.draw_circle_filled(position_cloud, 500, 50, arcade.color.WHITE)
+    arcade.draw_circle_filled(position_cloud + 65, 500, 50, arcade.color.WHITE)
 
 def draw_rolling_hills():
     arcade.draw_circle_filled(50, -100, 400, arcade.color.GREEN)
@@ -21,18 +21,22 @@ def draw_tree():
     arcade.draw_rectangle_filled(100, 290, 25, 40, arcade.color.BROWN)
 
 def draw_sun():
-    arcade.draw_circle_filled()
+    arcade.draw_circle_filled(400, position_sun, 100, arcade.color.SUNSET_ORANGE)
 
-def on_draw():
-    global position_cloud, speed
+def on_draw(delta_time):
+    global position_cloud, position_sun, speed_cloud, speed_sun
     arcade.start_render()
     draw_tree()
+    draw_sun()
+    position_sun += speed_sun
     draw_rolling_hills()
-    draw_cloud(position_cloud)
-    position_cloud += speed
+    draw_cloud()
+    position_cloud += speed_cloud
 
+position_sun = 0
 position_cloud = 0
-speed = 1
+speed_sun = 1
+speed_cloud = 2
 
 def main():
     arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing with Functions")
@@ -40,14 +44,14 @@ def main():
     arcade.start_render()
 
    # call your draw functions
+    draw_sun()
     draw_cloud()
     draw_rolling_hills()
     draw_tree()
 
     # Finish and run
-    arcade.finish_render()
+    arcade.schedule(on_draw, 1 / 60)
     arcade.run()
-
 
 # Call the main function to get the program started.
 main()
